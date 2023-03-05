6994a6adc117d1d0a3e599b12cc90dedb5f81187
Pedro Enrique Hermosilla <91793580+penriquehermosilla@users.noreply.github.com>
2023-03-04 21:38:01 -0500

    Add files via upload

---
+++ creditos.php
@@ -0,0 +1,8 @@
+<a href="https://www.flaticon.es/iconos-gratis/cerebro" title="cerebro iconos">Cerebro iconos creados por yoyonpujiono - Flaticon</a>
+<a href="https://www.flaticon.es/iconos-gratis/sistema-respiratorio" title="sistema respiratorio iconos">Sistema respiratorio iconos creados por Freepik - Flaticon</a>
+<a href="https://www.flaticon.es/iconos-gratis/disfrutar" title="disfrutar iconos">Disfrutar iconos creados por iconixar - Flaticon</a>
+<a href="https://www.flaticon.es/iconos-gratis/estomago" title="estómago iconos">Estómago iconos creados por Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/bones" title="bones icons">Bones icons created by Vitaly Gorbachev - Flaticon</a>
+<a href="https://www.flaticon.es/iconos-gratis/calorias" title="calorías iconos">Calorías iconos creados por Freepik - Flaticon</a>
+<a href="https://www.flaticon.es/iconos-gratis/columna-espinal" title="columna espinal iconos">Columna espinal iconos creados por Freepik - Flaticon</a>
+<a href="https://www.flaticon.es/iconos-gratis/articulaciones" title="articulaciones iconos">Articulaciones iconos creados por kerismaker - Flaticon</a>
